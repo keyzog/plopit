4a1c4a420689d8a4e9f87a4cca94b802ef968958
keyzog <soofft91@gmail.com>
2024-06-29 23:13:04 +0300

    remove logs

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,17 +1,9 @@
 #!/usr/bin/env node
 
-console.log("zcxv");
-
 const args = process.argv.slice(2);
-// import { parseArgs } from "../libs";
-// import path from "path";
 import { Plop, run } from "plop";
 import minimist from "minimist";
 const argv = minimist(args);
-
-console.log("lol kek");
-
-console.log("argv", args, argv);
 
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
@@ -20,11 +12,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const configPath = join(__dirname, "../plopfile.js");
-console.log("configPath", configPath);
 
 // Получаем текущий рабочий каталог, из которого был вызван скрипт
 const invokeDir = process.cwd();
-console.log("invokeDir", invokeDir);
 
 Plop.prepare(
 	{
